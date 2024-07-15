8e1262947c7783259809d4313839ef6c55441048
rosekamau <kamaurose236@gmail.com>
2024-07-16 02:10:09 +0300

    code

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-// fetch("http://localhost:3000/films")
-// .then(response=> response.json())
-// .then(films => 
-// films.map(film =>{
-//     const title = document.createElement('h1')
-//     title.inner =film.title
-//     main.appendchild(title)
-//     const image=document.createElement("img")
-//     image.src=film.poster
-//     main.appendchild(image)
-//     const description=document.createElement("p")
-//     description.textContent=film.description
-//     main.appendchild(description)
 
-
-// })
-
-// )
 
 fetch ("http://localhost:3000/films")
 .then(response => response.json())
