77f17241cea285771460f917ad5762ead948f6ac
rosekamau <kamaurose236@gmail.com>
2024-07-17 00:07:40 +0300

    week 3 code challenge

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # week-3-code-challenge
+The script.js , functions found perform the following:
+-It Fetches film data from the server.
+-Iterates over the list of films and creates HTML elements for each film's details.
+-Appends these elements to the main container in the HTML.
+-Adds an event listener to the form to handle ticket purchases.
+
--- script.js
+++ script.js
@@ -1,25 +1,58 @@
-
-
-fetch ("http://localhost:3000/films")
+fetch("http://localhost:3000/films")
 .then(response => response.json())
-.then(films => 
-  films.map(film=>{
-  const title= document.createElement("h1")
-  title.innerHTML = film.title
-  main.appendChild(title)
+.then(films => {
+ const main = document.getElementById('main');
+        
+ films.forEach(film => {
+            
+ const title = document.createElement("h1");
+ title.innerHTML = film.title;
+ main.appendChild(title);
 
-  const description=document.createElement("p")
-  description.innerHTML=film.description
-  main.appendChild(description)
+            
+ const description = document.createElement("p");
+ description.innerHTML = film.description;
+ main.appendChild(description);
 
-  const showtime =document.createElement("h3")
-  showtime.innerHTML=film.showtime
-  main.appendChild(showtime)
+            
+ const showtime = document.createElement("h3");
+ showtime.innerHTML = `Showtime: ${film.showtime}`;
+ main.appendChild(showtime);
 
-  const image=document.createElement("img")
-  image.src=film.poster
-  main.appendChild(image)
- 
-  
-}) 
-)
+            
+ const image = document.createElement("img");
+ image.src = film.poster;
+ main.appendChild(image);
+
+
+            
+ const form = document.createElement("form");
+ form.classList.add("ticket-form");
+
+            
+ const label = document.createElement("label");
+ label.innerHTML = "Number of tickets:";
+ form.appendChild(label);
+
+ const input = document.createElement("input");
+ input.type = "number";
+ input.min = "1";
+ input.value = "1";
+ input.name = "ticketCount";
+ form.appendChild(input);
+
+ const button = document.createElement("button");
+ button.type = "submit";
+ button.innerHTML = "Buy Tickets";
+ form.appendChild(button);
+
+ form.addEventListener("submit", (event) => {
+ event.preventDefault();
+ const ticketCount = input.value;
+ alert(`You have picked ${ticketCount} ticket(s) for ${film.title}`);
+});
+
+ main.appendChild(form);
+});
+})
+.catch(error => console.error('Error fetching films:', error));
